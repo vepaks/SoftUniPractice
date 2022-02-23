eb2c9eac27d745a9d670199d5855bad379b4e11d
Aleksandar Kamburov <kamburoff@gmail.com>
2022-02-23 08:05:34 +0200

    23.02.2022

--- Fundamentals JS/05.Data Types and Variables - More Exercise/01. Sum Digits.js
+++ Fundamentals JS/05.Data Types and Variables - More Exercise/01. Sum Digits.js
@@ -1,6 +1,14 @@
-function getNum (num) {
-let a = 5;
-                            console.log(a);
+function getSum(num) {
+    let sum = 0;
+    let string = num.toString();
+
+    for (let index = 0; index < string.length; index++) {
+        const element = Number(string[index]);
+        sum += element;
+    }
+    console.log(sum);
 }
 
-getNum(245678);
+getSum(245678);
+getSum(97561);
+getSum(543);
